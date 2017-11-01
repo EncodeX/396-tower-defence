2c1bbd96a9510c3143eefda074231a051672fa40
FrankGee <fang.254@osu.edu>
2017-11-01 18:00:54 -0500

    1800

--- Assets/code/enemyManager.cs
+++ Assets/code/enemyManager.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class NewMonoBehaviour : MonoBehaviour
 {
-	// Use this for initialization
-	void Start()
-	{
+    private Transform _holder;
+    private Object _NormalEnemy;
 
-	}
+    public void enemyManager(Transform holder)
+    {
+        _holder = holder;
+        _NormalEnemy = Resources.Load("Normal_Enemy");
+    }
 
-	// Update is called once per frame
-	void Update()
-	{
-			
-	}
+    public void ForceSpawn(Vector2 pos, Quaternion rotation, Vector2 velocity, float deathtime)
+    {
+        GameObject go = (GameObject)Object.Instantiate(_NormalEnemy, pos, rotation);
+        Bullet bul = go.GetComponent<Bullet>();
+        bul.Initialize(velocity, deathtime);
+    }
 }
