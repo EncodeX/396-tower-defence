757ee1c2b1339188e7dc9526d70da787f3b6450c
Encode.X <encode.xii@gmail.com>
2017-11-01 18:15:07 -0500

    base 基础完成

---
+++ Assets/Code/BaseHealthBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseHealthBar : MonoBehaviour {
+	private RectTransform _baseHealthBarRect;
+	private Slider _slider;
+
+	// Use this for initialization
+	void Start () {
+		_baseHealthBarRect = GetComponent<RectTransform>();
+		_slider = GetComponent<Slider>();
+		_slider.normalizedValue = 1f;
+		_slider.maxValue = 100f;
+		_slider.minValue = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var height = Screen.height * .5f;
+		var width = Screen.width * .5f;
+		_baseHealthBarRect.anchoredPosition = new Vector2(-width, height);
+	}
+
+	public void PerformDamage(float points) {
+		_slider.value -= points;
+	}
+}
---
+++ Assets/Code/Game.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour {
+    public static Game Ctx;
+    
+    public BaseHealthBar BaseHealthBar;
+
+    private void Start() {
+        Ctx = this;
+        BaseHealthBar = GameObject.Find("BaseHealthBar").GetComponent<BaseHealthBar>();
+    }
+}
